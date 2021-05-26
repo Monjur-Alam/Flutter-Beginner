728e67af0827546dd2430e1d51881abf5e7daa90
Munjur-Alam <munjur533@gmail.com>
2021-05-26 20:31:18 +0600

    Button & Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(MaterialApp(
-        home: Home()
-    ));
+void main() => runApp(MaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-            'My First App'
-        ),
+        title: Text('My First App'),
         centerTitle: true,
         backgroundColor: Colors.red[500],
       ),
       body: Center(
-        child: Image.asset('assets/a.png'),
-          // image: NetworkImage('https://image.winudf.com/v2/image/dHcudXJsLm9tZWdhLnNjcmVlbnNpemVfc2NyZWVuc2hvdHNfMV9lNTlhYWI4Zg/screen-1.jpg?fakeurl=1&type=.jpg'),
-          // image: AssetImage('assets/android.png'),
-        // ),
-      ),
+          child: IconButton(
+            onPressed: () {
+              print('you clicked me');
+            },
+            icon: Icon(Icons.alternate_email),
+            color: Colors.amber,
+          )
+
+          // RaisedButton.icon(
+          //   onPressed: () {},
+          //   icon: Icon(Icons.mail),
+          //   label: Text('mail me'),
+          //   color: Colors.amber,
+          // ),
+
+          // RaisedButton(
+          //   onPressed: () {
+          //     print('you clicked me');
+          //   },
+          //   child: Text('Click Me'),
+          //   color: Colors.blue,
+          // ),
+
+          // Icon(
+          //   Icons.account_circle_outlined,
+          //   color: Colors.amber,
+          //   size: 100.0,
+          // ),
+          ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('Click'),
