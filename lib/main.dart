39dd5a7c0306e04f7717907a5e5ae7a104a346e8
ashfaqurrahman <Ash@114431>
2021-05-27 11:34:31 +0600

    Containers & Padding 2/2

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,19 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[500],
       ),
-      body: Container(
-        color: Colors.grey[400],
+      body: Padding(
+        padding: EdgeInsets.all(100.0), // margin not working here
         child: Text('Container & Padding'),
       ),
+
+      // Container(
+      //   margin: EdgeInsets.all(30.0),
+      //   padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+        // padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30),
+        // padding: EdgeInsets.all(50.0),
+        // color: Colors.grey[400],
+        // child: Text('Container & Padding'),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('Click'),
