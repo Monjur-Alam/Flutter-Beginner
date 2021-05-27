f3adae578978dc59a8cf29cefa478ddde4b3cb0c
Munjur-Alam <munjur533@gmail.com>
2021-05-27 08:37:54 +0600

    Containers & Padding 1/2

--- lib/main.dart
+++ lib/main.dart
@@ -11,36 +11,10 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[500],
       ),
-      body: Center(
-          child: IconButton(
-            onPressed: () {
-              print('you clicked me');
-            },
-            icon: Icon(Icons.alternate_email),
-            color: Colors.amber,
-          )
-
-          // RaisedButton.icon(
-          //   onPressed: () {},
-          //   icon: Icon(Icons.mail),
-          //   label: Text('mail me'),
-          //   color: Colors.amber,
-          // ),
-
-          // RaisedButton(
-          //   onPressed: () {
-          //     print('you clicked me');
-          //   },
-          //   child: Text('Click Me'),
-          //   color: Colors.blue,
-          // ),
-
-          // Icon(
-          //   Icons.account_circle_outlined,
-          //   color: Colors.amber,
-          //   size: 100.0,
-          // ),
-          ),
+      body: Container(
+        color: Colors.grey[400],
+        child: Text('Container & Padding'),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('Click'),
