216fbc7ce2432b79fc8ee27b0e2b50276fdd56b2
Munjur-Alam <munjur533@gmail.com>
2021-05-29 17:57:27 +0600

    Formatting & showing data

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,15 +18,40 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       body: SafeArea(
-          child: Column(
-            children: [
-              TextButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/location');
-                  },
-                  icon: Icon(Icons.edit_location),
-                label: Text('Edit Location'),)
-            ],
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 120.0, 0.0, 0.0),
+            child: Column(
+              children: [
+                TextButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    icon: Icon(Icons.edit_location),
+                  label: Text('Edit Location'),
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      mapData['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  mapData['time'],
+                  style: TextStyle(
+                      fontSize: 48.0,
+                      letterSpacing: 2.0
+                  ),
+                ),
+              ],
+            ),
           )
       ),
     );
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   late String location; //location name for the UI
@@ -15,12 +16,12 @@ class WorldTime {
       Map data = jsonDecode(response.body);
       String dateTime = data['datetime'];
       String offset = data['utc_offset'].substring(1,3);
-      // print(dateTime);
-      // print(offset);
+      print(dateTime);
+      print(offset);
 
       DateTime now = DateTime.parse(dateTime);
       now = now.add(Duration(hours: int.parse(offset)));
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     }
     catch (e){
       print('Caught error: $e');
